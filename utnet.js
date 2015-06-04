a32be659071d6793ef108495e4a099c671d937b5
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2015-06-04 18:09:21 +0300

    phase two and major fixes

--- utnet.js
+++ utnet.js
@@ -1,26 +1,100 @@
+var bus = {};
+var log = {};
 var utTemplate = require('ut-template');
+var triggerError = require('../impl-ifinance/helpers').triggerError; // TODO fix this
 var _ = require('lodash');
+var templates = null;
+
+function login(auth, templates, self) {
+    var promise = null;
+    var lang = auth.language;
+    if (auth.fingerPrint) {
+        if (auth.customerNo) {
+            return bus.importMethod('bio.verifyClient')({
+                fingerPrint: auth.fingerPrint
+            });
+        } else {
+            promise = bus.importMethod('bio.biometricsLogin')({
+                fingerPrint: auth.fingerPrint
+            }).then(function(response) {
+                return this.execTemplateRow(templates.getUserProfile, auth);
+            }.bind(this));
+            if (auth.sessionData) {
+                return promise.then(function(response) {
+                    return this.execTemplateRow(templates.setActiveUserSession, auth);
+                });
+            } else {
+                return promise;
+            }
+        }
+    } else if (auth.username && auth.password) {
+        promise = self.execTemplateRow(templates.credentialsLogin, auth)({
+            username: auth.username,
+            password: auth.password
+        })
+        if (auth.sessionData) {
+            return promise.then(function(response) {
+                return this.execTemplateRow(templates.setActiveUserSession, auth);
+            }.bind(this));
+        } else {
+            return promise;
+        }
+    } else {
+        triggerError(null, lang, error);
+    }
+};
 
 module.exports = function(templates) {
     templates = _.assign({
-        check:utTemplate.load(require.resolve('./utnet/check.sql.marko')),
-        closeSession:utTemplate.load(require.resolve('./utnet/closeSession.sql.marko')),
-        invalidateSession:utTemplate.load(require.resolve('./utnet/invalidateSession.sql.marko')),
-        changePassword:utTemplate.load(require.resolve('./utnet/changePassword.sql.marko'))
+        check: utTemplate.load(require.resolve('./utnet/utNet_check.sql.marko')),
+        changeUserPassword: utTemplate.load(require.resolve('./utnet/changeUserPassword.sql.marko')),
+        changeUserPassword: utTemplate.load(require.resolve('./utnet/changeUserPassword.sql.marko')),
+        reloadSession: utTemplate.load(require.resolve('./utnet/reloadSession.sql.marko')),
+        updateSession: utTemplate.load(require.resolve('./utnet/updateSession.sql.marko'))
     }, templates);
 
     return {
+        init: function(b) {
+            bus = b;
+            log = bus.logFactory.createLog('warn', {name: 'identity', context: 'check'});
+        },
         check: function(auth) {
-            return this.execTemplateRow(templates.check, auth);
+            auth = auth.$$.authentication;
+            auth.implementationID = bus.config.implementation || '';
+            return this.execTemplateRow(templates.check, auth).then(function(response) {
+                return response;
+            }).catch(function(error) {
+                return error;
+            });
+            /*
+            if (auth.sessionId) {
+                return this.execTemplateRow(templates.sessionLogin, auth)({
+                    sessionId: auth.sessionId
+                }).then(function(response) {
+
+                }).catch(function() {
+                    return login(auth, templates, this);
+                });
+            } else {
+                return login(auth, templates, this);
+            }
+            */
         },
         closeSession: function(criteria) {
-            return this.execTemplateRow(templates.closeSession, criteria);
+            return this.execTemplateRow(templates.deleteActiveUserSession, criteria);
         },
-        invalidateSession: function(criteria) {
-            return this.execTemplateRow(templates.invalidateSession, criteria);
+        relaodSession: function(criteria) {
+            return this.execTemplateRow(templates.reloadSession, criteria);
         },
         changePassword: function(auth) {
-            return this.execTemplateRow(templates.changePassword, auth);
-        }
+            return this.execTemplateRow(templates.changeUserPassword, auth);
+        },
+        updateSession: function(msg) {
+            return this.execTemplateRow(templates.updateSession, auth);
+        },
     };
 };
+
+require('./validation.utnet')(module.exports);
+
+
---
+++ validation.utnet.js
@@ -0,0 +1,22 @@
+var joi = require('joi');
+var tags = ['api', 'identity'];
+var $$ = joi.object();
+
+var validation = {
+    check: {
+        tags: tags,
+        request: {
+            asdasdas: joi.string().required()
+        },
+        response: {
+
+        }
+    }
+};
+
+module.exports = function(module, ns) {
+    var _ = require('lodash');
+    Object.keys(module).forEach(function (value) {
+        _.assign(module[value], ns ? validation[ns][value] : validation[value]);
+    })
+};
