6eaa54a78861449c5f81c6ac6aa5da61c01a59d9
stamen.peev <stamen.peev@softwaregroup-bg.com>
2015-08-12 09:44:40 +0300

    move getParams inside module.exports in order for it to work correctly when multiple ut-identity/utswitch instances are instantiated simultaneously

--- utswitch.js
+++ utswitch.js
@@ -7,30 +7,6 @@ function getHash(user, pass) {
     md5.update(user, 'utf8');
     return md5.digest('hex').toString('hex').toUpperCase();
 }
-function getParams(params) {
-    var config =  _.assign({ // merge only once
-        'sessionTimeout'        : 600,
-        'singleUserSession'     : 'false',
-        'module'                : 'ut5',
-        'language'              : 'EN',
-        'remoteIp'              : null,
-        'implementation'        : 'default',
-        'userAgent'             : null,
-        'checkUserRightsIp'     : 'true',
-        'createSession'         : 'true'
-    }, (this.config && this.config.identity) || {});
-    getParams = function(params) { // lazy initialization
-        params.random = Math.random().toString(36).substring(5).toUpperCase();
-        if (params.password) {
-            params.passwordHash = getHash(params.username, params.password);
-        }
-        if (params.passwordNew) {
-            params.passwordHashNew = getHash(params.username, params.passwordNew);
-        }
-        return _.defaults(params, config);
-    }
-    return getParams(params);
-}
 module.exports = function(templates) {
     templates = _.assign({
         check:utTemplate.load(require.resolve('./utswitch/check.sql.marko')),
@@ -39,6 +15,30 @@ module.exports = function(templates) {
         changePassword:utTemplate.load(require.resolve('./utswitch/changePassword.sql.marko'))
     }, templates || {});
 
+    function getParams(params) {
+        var config =  _.assign({ // merge only once
+            'sessionTimeout'        : 600,
+            'singleUserSession'     : 'false',
+            'module'                : 'ut5',
+            'language'              : 'EN',
+            'remoteIp'              : null,
+            'implementation'        : 'default',
+            'userAgent'             : null,
+            'checkUserRightsIp'     : 'true',
+            'createSession'         : 'true'
+        }, (this.config && this.config.identity) || {});
+        getParams = function(params) { // lazy initialization
+            params.random = Math.random().toString(36).substring(5).toUpperCase();
+            if (params.password) {
+                params.passwordHash = getHash(params.username, params.password);
+            }
+            if (params.passwordNew) {
+                params.passwordHashNew = getHash(params.username, params.passwordNew);
+            }
+            return _.defaults(params, config);
+        }
+        return getParams(params);
+    }
     return {
         check: function(params) {
             return this.execTemplateRow(templates.check, getParams.call(this, params));
