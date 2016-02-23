d878c89f0fbddb76c2c1c3c8a1bbfc0458d5d60c
Tanya Rasheva <tanya.rasheva@softwaregroup-bg.com>
2016-02-23 14:50:31 +0200

    login functionallyty

--- ut-user/schema/750$identity.check.sql
+++ ut-user/schema/750$identity.check.sql
@@ -1,24 +1,95 @@
 ALTER PROCEDURE [identity].[check]
      @username NVARCHAR(200),
-     @password varchar(max),
-     @portName NVARCHAR(100),
-     @StartTime TIME(0) = NULL,
-     @DeviceID NVARCHAR(100) = NULL
+     @password varchar(max)
+     --@portName NVARCHAR(100),
+     --@StartTime TIME(0) = NULL,
+     --@DeviceID NVARCHAR(100) = NULL
 AS
 
 SET NOCOUNT ON;
 
+
 BEGIN TRY
-    SELECT
-        actorId
-    FROM
-        [user].hash
-    WHERE
-        identifier=@username
-        AND [value]=@password
+    DECLARE @UserID BIGINT,
+			@UserPassword NVARCHAR(50),
+			@LoginAttempts INT,
+			@MaxLoginAttempts INT = 3, 
+			@IsEnabled BIT = 0
+			
+    SELECT 
+        @UserID = h.actorId,
+        @UserPassword = h.value,
+        @LoginAttempts = h.failedAttempts,
+		@IsEnabled = h.isEnabled
+    FROM [user].[hash] AS h      
+    WHERE h.identifier = @UserName AND h.[type] = 'password'
+
+    -- check if the @UserID exists for this @UserName
+    IF @UserID IS NULL
+    BEGIN
+	    --INSERT INTO errorLog.BusinessErrorLog(ErrorCode, ErrorInfo, [Object], Params)
+     --   SELECT '4010', 'Invalid Credentials', 'LoginUser', 'userName: '+ @username +' password: '+ @password + ' portName: '+ @portName
+	    
+        raiserror('identy.invalid.credentials', 16, 1);
+    END
+	
+    --check if this credential is already locked
+    IF @IsEnabled = 0
+    BEGIN
+        --INSERT INTO errorLog.BusinessErrorLog(ErrorCode, ErrorInfo, [Object], Params)
+        --SELECT '4013', 'This Credential Is Locked', 'LoginUser', 'userName: '+ @username +' password: '+ @password + ' portName: '+ @portName
+	    
+	    raiserror('identy.disabled.credentials', 16, 1);
+    END
+
+    -- the Passwords do not match. If so, increment LoginAttempts
+    IF @UserID IS NOT NULL AND @UserPassword <> @Password
+    BEGIN
+        IF @LoginAttempts + 1 >= @MaxLoginAttempts -- In this case we need to lock this credential.
+			SET @IsEnabled = 0
+		   
+        UPDATE [user].[hash]
+            SET failedAttempts = @LoginAttempts + 1,
+                isEnabled = @IsEnabled
+        WHERE identifier = @UserName AND [type] = 'password'
+
+		IF @IsEnabled = 0
+		BEGIN
+			--INSERT INTO errorLog.BusinessErrorLog(ErrorCode, ErrorInfo, [Object], Params)
+			--SELECT '4013', 'This Credential Is Locked', 'LoginUser', 'userName: '+ @username +' password: '+ @password + ' portName: '+ @portName
+	    
+			raiserror('identity.the.credentials.locked', 16, 1);
+		END
+		ELSE
+		BEGIN
+			--INSERT INTO errorLog.BusinessErrorLog(ErrorCode, ErrorInfo, [Object], Params)
+			--SELECT '4012', 'Wrong Password', 'LoginUser', 'userName: '+ @username +' password: '+ @password + ' portName: '+ @portName
+	    
+			raiserror('identity.wrong.password', 16, 1);
+		END
+    END
+
+    IF @LoginAttempts > 0
+    BEGIN
+		UPDATE [user].[hash]
+            SET failedAttempts = 0,
+			lastAttempt = getdate()
+        WHERE identifier = @UserName AND [type] = 'password'
+    END
+
+	SELECT actorId
+    FROM [user].hash
+    WHERE identifier=@username and type = 'password'
+               
+	RETURN      
 END TRY
 BEGIN CATCH
-   IF @@trancount > 0 ROLLBACK TRANSACTION
-   EXEC error_handler_sp
-   RETURN 55555
+	declare @errorMessage nvarchar(1000) = ERROR_MESSAGE(), @errorSeverity int = ERROR_SEVERITY(), @errorState int = ERROR_STATE()
+    DECLARE @errorInfo NVARCHAR(MAX) = ''
+    EXEC dbo.ErrorInfoGet @errorInfo OUTPUT
+        
+    INSERT INTO dbo._ErrorLog(ErrorInfo, Params)
+    SELECT @errorInfo, 'userName: '+ @username +' password: '+ @password 
+    
+	raiserror(@errorMessage, @errorSeverity, @errorState);
 END CATCH
