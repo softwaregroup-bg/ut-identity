1f6cabcaa7e1d0d10f1c4e329996a244f937a251
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-06-07 14:00:09 +0300

    fix: set meta methods explicitly before calling stored procedures

--- ut-user/index.js
+++ ut-user/index.js
@@ -14,15 +14,18 @@ module.exports = {
         genHash = this.bus.importMethod('user.genHash');
         if (msg.fingerprints && msg.fingerprints.length > 0) {
             // bio logic
-            get = this.bus.importMethod('user.identity.get')(msg, $meta)
+            $meta.method = 'user.identity.get';
+            get = this.bus.importMethod($meta.method)(msg, $meta)
             .then((r) => {
-                return this.bus.importMethod('bio.check')({data: msg.fingerprints, bioId: r[0][0].bioid}, $meta);
+                $meta.method = 'bio.check';
+                return this.bus.importMethod($meta.method)({data: msg.fingerprints, bioId: r[0][0].bioid}, $meta);
             })
             .then((r) => ({bioid: r.bioId, username: msg.username, actionId: msg.actionId}));
         } else if (msg.sessionId) {
             get = Promise.resolve(msg);
         } else {
-            get = this.bus.importMethod('user.identity.get')(msg, $meta)
+            $meta.method = 'user.identity.get';
+            get = this.bus.importMethod($meta.method)(msg, $meta)
             .then((userParams) => {
                 var hashQueue = userParams.hashParams
                 .filter((hp) => (msg[hp.type]))
@@ -49,13 +52,15 @@ module.exports = {
 
         return get
             .then((r) => {
-                return this.bus.importMethod('user.identity.check')(r, $meta)
+                $meta.method = 'user.identity.check';
+                return this.bus.importMethod($meta.method)(r, $meta)
                 .then((user) => {
                     if (user.loginPolicy && user.loginPolicy.length > 0) {
                         return {loginPolicy: user.loginPolicy};
                     }
                     if (!user['permission.get']) { // in case user.identity.check did not return the permissions
-                        return this.bus.importMethod('permission.get')({actionId: msg.actionId},
+                        $meta.method = 'permission.get';
+                        return this.bus.importMethod($meta.method)({actionId: msg.actionId},
                             {actorId: user['identity.check'].userId, actionId: 'identity.check'})
                             .then((permissions) => {
                                 user['permission.get'] = permissions && permissions[0];
@@ -70,13 +75,16 @@ module.exports = {
             });
     },
     closeSession: function(msg, $meta) {
-        return this.bus.importMethod('user.session.delete')({sessionId: $meta.auth.sessionId}, $meta);
+        $meta.method = 'user.session.delete';
+        return this.bus.importMethod($meta.method)({sessionId: $meta.auth.sessionId}, $meta);
     },
     changePassword: function(msg, $meta) {
-        return this.bus.importMethod('user.identity.get')({userId: $meta.auth.actorId}, $meta)
+        $meta.method = 'user.identity.get';
+        return this.bus.importMethod($meta.method)({userId: $meta.auth.actorId}, $meta)
         .then((r) => {
             msg.hashParams = r.hashParams[0];
-            return this.bus.importMethod('user.changePassword')(msg, $meta);
+            $meta.method = 'user.changePassword';
+            return this.bus.importMethod($meta.method)(msg, $meta);
         });
     }
 };
