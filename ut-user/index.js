86786f87770f1e22ecf225da0d3866dccf38f3c4
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-06-21 14:28:18 +0300

    fix: policy refactoring prepared

--- ut-user/index.js
+++ ut-user/index.js
@@ -1,113 +1,107 @@
 var errors = require('../errors');
-var genHash;
+var bus;
 function getHash(password, hashInfo) {
     if (!hashInfo || !hashInfo.params) {
         return errors.MissingCredentials.reject();
     }
     hashInfo.params = typeof (hashInfo.params) === 'string' ? JSON.parse(hashInfo.params) : hashInfo.params;
-    return genHash(password, hashInfo.params);
+    return bus.importMethod('user.genHash')(password, hashInfo.params);
 }
+var hashMethods = {
+    otp: function(value, hashParams) {
+        return getHash(value, hashParams);
+    },
+    password: function(value, hashParams) {
+        return getHash(value, hashParams);
+    },
+    newPassword: function(value, hashParams) {
+        return getHash(value, hashParams);
+    },
+    bio: function(value, hashParams) {
+        var params = JSON.parse(hashParams);
+        return bus.importMethod('bio.check')({
+            id: params.id,
+            departmentId: params.departmentId,
+            data: value
+        })
+        .then(function(r) {
+            return 1;
+        })
+        .catch(function(r) {
+            return 0;
+        });
+    }
+};
 
 module.exports = {
+    init: function(b) {
+        bus = b;
+    },
     check: function(msg, $meta) {
+        delete msg.type;
         var get;
-        genHash = this.bus.importMethod('user.genHash');
-        delete msg.bio;
-        if (msg.fingerprints) {
-            // bio logic
-            $meta.method = 'user.identity.get';
-            get = this.bus.importMethod($meta.method)(msg, $meta)
-            .then((r) => {
-                var params = r.hashParams[0].params && JSON.parse(r.hashParams[0].params);
-                if (!params) {
-                    $meta.mtid = 'error';
-                    return {
-                        code: 4444,
-                        message: 'User is not bio enrolled'
-                    };
-                }
-                $meta.method = 'bio.check';
-                return this.bus.importMethod($meta.method)({
-                    id: params.id,
-                    departmentId: params.departmentId,
-                    data: msg.fingerprints
-                }, $meta)
-                    .then(function(r) {
-                        msg.bio = 1;
-                        return msg;
-                    })
-                    .catch(function(r) {
-                        msg.bio = 0;
-                        return msg;
-                    });
-            });
-        } else if (msg.sessionId) {
+        if (msg.sessionId) {
             get = Promise.resolve(msg);
         } else {
-            $meta.method = 'user.identity.get';
-            get = this.bus.importMethod($meta.method)(msg, $meta)
-            .then((userParams) => {
-                var hashQueue = userParams.hashParams
-                .filter((hp) => (msg[hp.type]))
-                .map((hp) => {
-                    var hashValue = msg[hp.type]; // what to hash, otp or password
-                    return getHash(hashValue, hp)
-                    .then((oldHash) => {
-                        msg[hp.type] = oldHash;
-                        if (msg.newPassword && hp.type === 'password') { // change password case
-                            return getHash(msg.newPassword, hp)
-                            .then((newHash) => {
-                                msg.newPassword = newHash;
-                                return msg;
-                            });
-                        }
+            $meta.method = 'user.identity.get'; // get hashes info
+            get = bus.importMethod($meta.method)(msg, $meta)
+                .then(function(result) {
+                    if (!result.hashParams) {
+                        throw new Error('no hash params');
+                    }
+                    var hashParams = result.hashParams.reduce(function(all, record) {
+                        all[record.type] = record.params;
+                        return all;
+                    }, {});
+                    return Promise.all(
+                        Object.keys(hashMethods)
+                            .filter(function(method) {
+                                return hashParams[method] && msg[method];
+                            })
+                            .map(function(method) {
+                                return hashMethods[method](msg[method], hashParams[method])
+                                    .then(function(value) {
+                                        msg[method] = value;
+                                    });
+                            })
+                    )
+                    .then(function() {
                         return msg;
                     });
                 });
-
-                return Promise.all(hashQueue)
-                .then(() => (msg));
-            });
         }
-
         return get
-            .then((r) => {
+            .then(function(r) {
                 $meta.method = 'user.identity.check';
-                return this.bus.importMethod($meta.method)(r, $meta)
-                .then((user) => {
-                    if (user.loginPolicy && user.loginPolicy.length > 0) {
-                        return {loginPolicy: user.loginPolicy};
-                    }
-                    if (!user['permission.get']) { // in case user.identity.check did not return the permissions
-                        $meta.method = 'permission.get';
-                        return this.bus.importMethod($meta.method)({actionId: msg.actionId},
-                            {actorId: user['identity.check'].userId, actionId: 'identity.check'})
-                            .then((permissions) => {
-                                user['permission.get'] = permissions && permissions[0];
-                                return user;
-                            });
-                    }
-                    return user;
-                });
-            })
-            .catch((err) => {
-                throw new errors.InvalidCredentials(err);
+                return bus.importMethod($meta.method)(r, $meta)
+                    .then(function(user) {
+                        if ((!user.loginPolicy || !user.loginPolicy.length) && !user['permission.get']) { // in case user.identity.check did not return the permissions
+                            $meta.method = 'permission.get';
+                            return bus.importMethod($meta.method)({actionId: msg.actionId},
+                                {actorId: user['identity.check'].userId, actionId: 'identity.check'})
+                                .then((permissions) => {
+                                    user['permission.get'] = permissions && permissions[0];
+                                    return user;
+                                });
+                        }
+                        return user;
+                    });
             });
     },
     closeSession: function(msg, $meta) {
         $meta.method = 'user.session.delete';
-        return this.bus.importMethod($meta.method)({sessionId: $meta.auth.sessionId}, $meta);
+        return bus.importMethod($meta.method)({sessionId: $meta.auth.sessionId}, $meta);
     },
     changePassword: function(msg, $meta) {
         $meta.method = 'user.identity.get';
-        return this.bus.importMethod($meta.method)({
+        return bus.importMethod($meta.method)({
             userId: $meta.auth.actorId,
             type: 'password'
         }, $meta)
             .then((r) => {
                 msg.hashParams = r.hashParams[0];
                 $meta.method = 'user.changePassword';
-                return this.bus.importMethod($meta.method)(msg, $meta);
+                return bus.importMethod($meta.method)(msg, $meta);
             });
     }
 };
