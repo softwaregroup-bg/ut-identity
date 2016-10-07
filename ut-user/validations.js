ef0b87569a6ffd9e3df71467c72665517fe08ea9
elin.angelov <elin.angelov@softwaregroup-bg.com>
2016-10-07 13:26:49 +0300

    feat: joi validation added

---
+++ ut-user/validations.js
@@ -0,0 +1,16 @@
+'use strict';
+var joi = require('joi');
+
+module.exports = {
+    'check': {
+        description: 'identity check',
+        notes: ['identity check'],
+        tags: ['identity'],
+        params: joi.object({
+            username: joi.string().required()
+        }),
+        result: joi.object({
+            username: joi.number().integer()
+        })
+    }
+};
