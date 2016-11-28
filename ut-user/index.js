cac859673600943aebbe6d5c89d645235436c3f6
Simeon Petkov <simeon.petkov@softwaregroup-bg.com>
2016-11-28 15:35:10 +0200

    MCTUNE-1560: new logic for error

--- ut-user/index.js
+++ ut-user/index.js
@@ -233,7 +233,7 @@ module.exports = {
         }
         if (msg.hasOwnProperty('newPassword') && !msg.hasOwnProperty('registerPassword')) {
             if (msg.hasOwnProperty('forgottenPassword') && msg.hasOwnProperty('registerPassword')) {
-                throw new errors.SystemError('invalid.request');
+                throw errors['identity.systemError']('invalid.request');
             }
 
             // Validate new password access policy
