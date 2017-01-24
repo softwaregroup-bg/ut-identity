85091c3a7d592dc2ca17e335254732fd4a1c867d
tsvetanka.chipilova <tsvetanka.chipilova@softwaregroup-bg.com>
2017-01-24 08:41:57 +0200

    fix: UIS-311 get all errors

--- errors.js
+++ errors.js
@@ -128,7 +128,7 @@ module.exports = [
     }
 ].reduce(function(prev, next) {
     var spec = next.name.split('.');
-    var Ctor = create(spec.pop(), spec.join('.'), next.defaultMessage);
+    var Ctor = create(spec.pop(), spec.join('.'), next.defaultMessage, next);
     prev[next.name] = function(params) {
         return new Ctor({params: params});
     };
