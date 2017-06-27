95607a0a4aa31fe057a57ab58905bcf642cd2cab
Kanagu Duraiswamy <Kanagu.Duraiswamy@softwaregroup-bg.com>
2017-06-27 22:44:00 +1000

    feat: UIS-2550 Revert the audit log changes. Identity module doesnt require any audit log changes.

--- ut-user/index.js
+++ ut-user/index.js
@@ -2,15 +2,12 @@ var UtIdentityHelpers = require('./helpers');
 var assign = require('lodash.assign');
 var errors = require('../errors');
 var UtCrypt = require('./crypt');
-var path = require('path');
-var os = require('os');
 
 var helpers;
 var crypt;
 var importMethod;
 var checkMethod;
 var debug;
-var utVersion;
 
 function getCrypt(cryptKey) {
     if (!crypt) {
@@ -234,33 +231,15 @@ module.exports = {
         return get
             .then(function(r) {
                 $meta.method = checkMethod || 'user.identity.checkPolicy';
-                var secretQuestionAnswer = [];
                 if (msg.secretQuestion && msg.secretAnswer) {
-                    secretQuestionAnswer = {
+                    r.secretQuestionAnswer = {
                         actorId: r.actorId,
                         questionId: msg.secretQuestion,
                         answer: msg.secretAnswer
                     };
+                } else {
+                    r.secretQuestionAnswer = [];
                 }
-                
-                /*
-                Hiding this code as this seem not to be doing what it intends to do.
-                if (msg.lat && msg.lng) {
-                    r.lat = msg.lat;
-                    r.lng = msg.lng;
-                }
-                */
-
-                if ($meta.latitude && $meta.longitude) {
-                    r.latitude = $meta.latitude;
-                    r.longitude = $meta.longitude;
-                }
-
-                r.utVersion = utVersion;
-                r.osVersion = [os.type(), os.platform(), os.release()].join(':');
-                r.machineName = os.hostname();
-                r.destinationIPAddress = $meta.destinationIPAddress;
-                r.secretQuestionAnswer = secretQuestionAnswer;
                 return importMethod($meta.method)(r, $meta)
                     .then(function(user) {
                         if (user.pushNotificationToken && user.pushNotificationToken.pushNotificationToken !== undefined) {
